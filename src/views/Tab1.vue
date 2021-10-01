939866eaec7e0bdb1cc5606a004289efb7a1f495
BOURBON27 <4nci3n@gmail.com>
2021-10-01 09:55:41 +0200

    Last changes

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,47 +2,29 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Pass</ion-title>
+        <ion-title>Liste cryptos</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">QR code</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <div>
-        <ion-button color="primary">Primary</ion-button>
-        <ion-button color="secondary">Secondary</ion-button>
-        <ion-button color="tertiary">Tertiary</ion-button>
-        <ion-button color="success">Success</ion-button>
-        <ion-button color="warning">Warning</ion-button>
-        <ion-button color="danger">Danger</ion-button>
-        <ion-button color="light">Light</ion-button>
-        <ion-button color="medium">Medium</ion-button>
-        <ion-button color="dark">Dark</ion-button>
-      </div>
-       <ion-badge>99</ion-badge>
+<!-- List of cryptos -->
+      <ion-list>
+        <ion-item>
+       
+          <ion-label> <a href="https://coinmarketcap.com/fr/currencies/bitcoin/">Bitcoin</a></ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>VET</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>VTHO</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>ETH</ion-label>
+        </ion-item>
+      </ion-list>
 
-  <!-- Colors -->
-  <ion-badge color="primary">11</ion-badge>
-  <ion-badge color="secondary">22</ion-badge>
-  <ion-badge color="tertiary">33</ion-badge>
-  <ion-badge color="success">44</ion-badge>
-  <ion-badge color="warning">55</ion-badge>
-  <ion-badge color="danger">66</ion-badge>
-  <ion-badge color="light">77</ion-badge>
-  <ion-badge color="medium">88</ion-badge>
-  <ion-badge color="dark">99</ion-badge>
-
-  <!-- Item with badge on left and right -->
-  <ion-item>
-    <ion-badge slot="start">11</ion-badge>
-    <ion-label>My Item</ion-label>
-    <ion-badge slot="end">22</ion-badge>
-  </ion-item>
-
-      <ExploreContainer name="Tab 1 page" />
+  <!-- List of Input Items -->
+ 
     </ion-content>
   </ion-page>
 </template>
@@ -55,12 +37,11 @@ import {
   IonTitle,
   IonContent,
 } from "@ionic/vue";
-import ExploreContainer from "@/components/ExploreContainer.vue";
 
 export default {
   name: "Tab1",
   components: {
-    ExploreContainer,
+
     IonHeader,
     IonToolbar,
     IonTitle,
